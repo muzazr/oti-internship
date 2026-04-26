625611ad74f7a79d75c971a7d0ab88a8085eb6e4
Yes-07 <nichola.rucita12@gmail.com>
2026-04-26 13:03:10 +0700

    feat(general): added design system and assets

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,6 +6,12 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <QueryProvider>{children}</QueryProvider>
       </body>

c14081ec0644391361fb159e709e377e7824efae
muzazr <zakiazhar046@gmail.com>
2026-04-27 23:17:18 +0700

    feat: add routing

---
+++ backend/src/routes.js
@@ -0,0 +1,25 @@
+import express from "express"
+
+import whatsappRoutes from "./modules/whatsapp/whatsapp.routes.js"
+import authRoutes from "./modules/auth/auth.routes.js"
+import teachersRoutes from "./modules/teachers/teachers.routes.js"
+import classesRoutes from "./modules/classes/classes.routes.js"
+import studentsRoutes from "./modules/students/students.routes.js"
+import assignmentsRoutes from "./modules/assignments/assignments.routes.js"
+import uploadLinksRoutes from "./modules/upload-links/uploadLinks.routes.js"
+import submissionsRoutes from "./modules/submissions/submissions.routes.js"
+import reportsRoutes from "./modules/reports/reports.routes.js"
+
+const router = express.Router()
+
+router.use("/whatsapp", whatsappRoutes)
+router.use("/auth", authRoutes)
+router.use("/teachers", teachersRoutes)
+router.use("/classes", classesRoutes)
+router.use("/students", studentsRoutes)
+router.use("/assignments", assignmentsRoutes)
+router.use("/upload-links", uploadLinksRoutes)
+router.use("/submissions", submissionsRoutes)
+router.use("/reports", reportsRoutes)
+
+export default router
